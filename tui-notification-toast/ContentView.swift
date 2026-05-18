7872782a7b0e20b8f4ddf2de179f70dbdc1c1c9c
e <e>
2026-05-18 13:44:04 +0300

    Initial Commit

---
+++ tui-notification-toast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tui-notification-toast
+//
+//  Created by Pavel Korostelev on 18.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tui-notification-toast/tui_notification_toastApp.swift
@@ -0,0 +1,17 @@
+//
+//  tui_notification_toastApp.swift
+//  tui-notification-toast
+//
+//  Created by Pavel Korostelev on 18.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct tui_notification_toastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
